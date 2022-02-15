36dc1dee9401ac6d356884d7bb271691aae74449
BSY-Development <bes.yamamoto@gmail.com>
2022-02-15 15:02:35 -0300

    feat: back update title and status

--- server/src/controllers/list/router.js
+++ server/src/controllers/list/router.js
@@ -2,11 +2,13 @@ const express = require('express');
 const create = require('./create');
 const get = require('./get');
 const remove = require('./remove');
+const update = require('./update');
 
 const router = express.Router({ mergeParams: true });
 
 router.post('/', create);
 router.get('/', get);
 router.delete('/:id', remove);
+router.put('/:id', update);
 
 module.exports = router;
---
+++ server/src/models/document/update.js
@@ -0,0 +1,12 @@
+const { ObjectId } = require('mongodb');
+const connection = require('../connection');
+
+module.exports = async (id, { title, status }) => {
+  const result = await (await connection()).collection('list').updateOne(
+    {
+      "_id": ObjectId(id),
+    },
+    { $set: { title, status } },
+  );
+  return result;
+};
