91c1ee169d8ec160494eee4dc3c6581134771f07
BSY-Development <bes.yamamoto@gmail.com>
2022-02-16 15:45:10 -0300

    feat + css: creating footer and adding styles

---
+++ client/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Styles/footer.css';
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <div>
+        <h2>Bruno Yamamoto</h2>
+        <p>© Copyright 2022</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
