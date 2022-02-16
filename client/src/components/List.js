1e753c6c6078468f8a0bf2b5d17c372f14038b86
Bruno Yamamoto <bes.yamamoto@gmail.com>
2022-02-16 18:25:12 -0300

    [Front][Sort] (#4)
    Front end Sort

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -10,6 +10,7 @@ socket.emit('');
 function List() {
   const [data, setData] = useState([]);
   const [value, setValue] = useState('');
+  const [sortBy, setSortBy] = useState('date');
 
   useEffect(() => {
     const getData = async () => {
@@ -36,14 +37,64 @@ function List() {
     setValue('');
   };
 
+  const sortMethod = (a, b) => {
+    if (a[sortBy] > b[sortBy]) return 1;
+    if (b[sortBy] > a[sortBy]) return -1;
+
+    return 0;
+  };
+
   if(!data.length) {
-    return <h1>Loading...</h1>;
+    return (
+      <div>
+        <h1>Loading...</h1>
+        <div className='create-div'>
+          <h2>Create new</h2>
+          <input
+            className='create-input'
+            type="text"
+            value={ value }
+            onChange={ (e) => setValue(e.target.value) }
+          />
+          <button type="button" className='btn-update' onClick={ handleClick }>Add</button>
+        </div>
+      </div>
+    );
   }
 
   return (
     <div className='list-main'>
+      <div className='order-div'>
+        <h2>Order by:</h2>
+        <input
+          type="radio"
+          id="title"
+          name="sort"
+          value="title"
+          onClick={ () => setSortBy('title') }
+        />
+        <label htmlFor="title">Title</label>
+
+        <input
+          type="radio"
+          id="status"
+          name="sort"
+          value="status"
+          onClick={ () => setSortBy('status') }
+        />
+        <label htmlFor="status">Status</label>
+
+        <input
+          type="radio"
+          id="date"
+          name="sort"
+          value="date"
+          onClick={ () => setSortBy('date') }
+        />
+        <label htmlFor="date">Date</label>
+      </div>
       <ul>
-        { data.map((item, i) => <li key={ i } id={ item._id }>
+        { data.sort(sortMethod).map((item, i) => <li key={ i } id={ item._id }>
           <Card content={ item } />
         </li>) }
       </ul>
