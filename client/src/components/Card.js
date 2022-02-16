44ca3157151ae99fa6ca8f5c237ef797d9fe34a9
Bruno Yamamoto <bes.yamamoto@gmail.com>
2022-02-16 14:51:57 -0300

    [Front][CRUD + Socket] (#2)
    Front end interface + Socket

---
+++ client/src/components/Card.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
+socket.emit('');
+
+function Card({ content: {title, status, date, _id: id } }) {
+  const [doUpdate, setDoUpdate] = useState(false);
+  const [value, setValue] = useState(title);
+  const [newStatus, setNewStatus] = useState(status)
+
+  const handleUpdate = async (id) => {
+    await fetch(`http://localhost:3001/list/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: value, status: newStatus }),
+    });
+    setDoUpdate(false);
+    socket.emit('update');
+  }
+
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:3001/list/${id}`, {
+      method: 'DELETE',
+      headers: { 'Content-Type': 'application/json' },
+    });
+    socket.emit('update');
+  }
+
+  if (doUpdate) {
+    return (
+      <div>
+        <input type="text" value={ value } onChange={ (e) => setValue(e.target.value) } />
+
+        <input
+          type="radio"
+          id="pendente"
+          name="status"
+          value="Pendente"
+          onClick={ (e) => { setNewStatus(e.target.value) } }
+          defaultChecked={ status === 'Pendente' }
+        />
+        <label htmlFor="pendente">Pendente</label>
+
+        <input
+          type="radio"
+          id="andamento"
+          name="status"
+          value="Em andamento"
+          onClick={ (e) => { setNewStatus(e.target.value) } }
+          defaultChecked={ status === 'Em andamento' }
+        />
+        <label htmlFor="andamento">Em andamento</label>
+
+        <input
+          type="radio"
+          id="pronto"
+          name="status"
+          value="Pronto"
+          onClick={ (e) => { setNewStatus(e.target.value) } }
+          defaultChecked={ status === 'Pronto' }
+        />
+        <label htmlFor="pronto">Pronto</label>
+
+        <button type="button" onClick={ () => handleUpdate(id) }>Atualizar</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <p>{ title }</p>
+      <p>{ status }</p>
+      <p>{ date }</p>
+      <button type="button" onClick={ () => handleDelete(id) }>Deletar</button>
+      <button type="button" onClick={ () => setDoUpdate(true) }>Editar</button>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  content: PropTypes.shape({
+    title: PropTypes.string,
+    status: PropTypes.string,
+    date: PropTypes.string,
+    _id: PropTypes.string,
+  }).isRequired,
+};
+
+export default Card;
--- server/src/api/app.js
+++ server/src/api/app.js
@@ -1,8 +1,30 @@
 const express = require('express');
-const root = require('../controllers/root');
+const cors = require('cors');
 
 const app = express();
+const http = require('http').createServer(app);
+
+const io = require('socket.io')(http, {
+  cors: {
+      origin: 'http://localhost:3000',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+const root = require('../controllers/root');
+const error = require('../middleware/error');
+const get = require('../models/document/get');
+
+io.on('connection', (socket) => {
+  socket.on('update', async () => {
+    const result = await get();
+    io.emit('updateData', result);
+  });
+});
+
 app.use(express.json());
+app.use(cors());
+app.use(error);
 app.use('/', root);
 
-module.exports = app;
+module.exports = http;
